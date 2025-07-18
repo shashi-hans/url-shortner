9de9cd5126bcd71fdf71ca8159cc7d1e93db95bb
Shashi Hans <shashihans87@gmail.com>
2025-07-18 23:33:17 +0530

    update server rendering url

--- client/src/util.js
+++ client/src/util.js
@@ -1,5 +1,5 @@
 const baseURL = (window.location.hostname === "localhost") ?
-          "http://localhost:4000" : "https://urlshortner-oezp.onrender.com";
+          "http://localhost:4000" : "https://url-shortner-5wbk.onrender.com/";
 const end = '/api/url/shorten';
 export const baseUrl = `${baseURL}`;
 export const fullURL = `${baseURL}${end}`;
